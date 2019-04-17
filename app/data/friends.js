fc9476e5a016806b82988a77324eed62fca2a117
Puspa Sharma <blackice002@gmail.com>
2019-04-17 09:57:50 -0400

    update 12

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,7 @@
 var friendsArray = [{
-"name":"",
-"photo": "linkfile",
-"scores":[
+    "name":"Ghostface",
+    "photo": "https://inst-1.cdn.shockers.de/hs_cdn/out/pictures//master/product/1/skeleton-ghostface-maske--skelett-scream-maske--halloween-maske-29511.jpg",
+    "scores":["2","5","5","4","5","1","1","2","2","2"]
+    }]
     
-]
-
-
-
-}]
-
-module.exports = friendsArray;
+    module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-//dependencies
+// dependencies.
 var express = require('express');
-//express configuration
+var bodyParser = require('body-parser');
 
-var app =express();
+// Initialize app.
+var app = express();
+
 // port setup
-var PORT = process.argv.PORT ||8080;
+var PORT = process.env.PORT || 8080;
 
-// express app to handle data parsing
-app.use(express.urlencoded({extended:true, limit:'3mb'}));
-app.use(express.json({limit:'3mb'}));
-app.use(express.statis('public'));
+// Set up middleware.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(__dirname + '/app/public'));
 
-// router
+// Import routes.
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
-// It will effectively starts our server
+// server listener
 app.listen(PORT, function(){
 console.log("app listening on Port : http://localhost :"+ PORT)
 });
